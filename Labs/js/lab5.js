bf1e681eb6ceb8aab94d0432698fc9323b2ce0bb
Ram Niwash <rniwash122@gmail.com>
2024-10-06 18:26:11 +0530

    Added Lab 5

---
+++ Labs/js/lab5.js
@@ -0,0 +1,13 @@
+let age = 25;
+console.log("AGE :", age);
+age = 30;
+console.log("AGE :", age);
+
+const BIRTH_YEAR = 2001;
+console.log("BIRTH YEAR :", BIRTH_YEAR);
+// BIRTH_YEAR = 2005;
+
+var name = "RAM";
+console.log("NAME :", name);
+name = "RAM NIWASH";
+console.log("NAME :", name);
