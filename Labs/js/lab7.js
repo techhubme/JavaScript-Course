b6ff781ec34b9b54bf5d19183d379111c77e2a6a
Ram Niwash <rniwash122@gmail.com>
2024-10-19 22:10:02 +0530

    Added Lab7

---
+++ Labs/js/lab7.js
@@ -0,0 +1,9 @@
+let result = 7 - 2 + 5 * 10 / 2;
+console.log("RESULT = [ 7 - 2 + 5 * 10 / 2 ]", result);
+
+result = 4 - 2 + 60 / 10 * 2;
+console.log("RESULT = [ 4 - 2 + 60 / 10 * 2 ]", result);
+
+result = 7 - (2 + 5) * (10 / 2);
+console.log("RESULT = [ 7 - (2 + 5) * (10 / 2) ]", result);
+
