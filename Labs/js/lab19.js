dc3974be269c1fe7349dbabf76879d1d4f3d70b7
Ram Niwash <rniwash122@gmail.com>
2024-11-09 16:38:13 +0530

    Add lab 18

---
+++ Labs/js/lab19.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const PERSON = {
+    name:"Harish",
+    age:31,
+    profession:"Teacher",
+    mobileNumbers:[4567, 9876],
+    married:true,
+    address:{
+        locality:"Near Govt. School",
+        city:"Chandigarh",
+        state:"Haryana",
+        pinCode:123456,
+    }
+}
+
+console.log("PERSON = ",PERSON);
+console.log("TYPE OF PERSON = ",typeof PERSON);
