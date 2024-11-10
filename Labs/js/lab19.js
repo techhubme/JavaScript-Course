5bc700f49488dbaa5b703bac1a86bf9350598af9
Ram Niwash <rniwash122@gmail.com>
2024-11-10 16:29:39 +0530

    Updated Lab 19

--- Labs/js/lab19.js
+++ Labs/js/lab19.js
@@ -1,17 +1,61 @@
 'use strict';
 
 const PERSON = {
-    firstName:"Harish",
+
+    firstName:"Krishan",
+
     lastName:"Kumar",
+
     age:31,
+
     profession:"Teacher",
+
     mobileNumbers:[4567, 9876],
+
     married:true,
+
     address:{
+
         locality:"Near Govt. School",
+
         city:"Chandigarh",
+
         state:"Haryana",
+
         pinCode:123456,
+    },
+
+    getFirstName: function(){
+        return this.firstName;
+    },
+
+    getLastName: function(){
+        return this.lastName;
+    },
+
+    getAge: function(){
+        return this.age;
+    },
+
+    getProfession: function(){
+        return this.profession;
+    },
+
+    getMobileNumbers: function(){
+        return this.mobileNumbers;
+    },
+
+    isMarried: function(){
+        return this.married;
+    },
+
+    getAddress: function(){
+        return this.address;
+    },
+
+    getSummary: function(){
+        return `Hello my name is ${this.firstName} ${this.lastName} I am ${this.age} old and I am ${this.profession} by profession,
+        I stay at ${this.address.locality} in ${this.address.city} my twitter handle is ${this.twitter}`;
     }
 }
 
@@ -40,3 +84,12 @@ alert(personInfo);
 /** Add another Properity to Object */
 PERSON['twitter'] = '@hellojs'  // Only sample data
 console.log("PERSON = ",PERSON);
+
+/** Functions of the Object */
+console.log("PERSON NAME", PERSON.getFirstName(), PERSON.getLastName());
+console.log("PERSON AGE", PERSON.getAge());
+console.log("PERSON PROFESSION", PERSON.getProfession());
+console.log("PERSON MARRIED", PERSON.isMarried());
+console.log("PERSON MOBILENUMBERS", PERSON.getMobileNumbers());
+console.log("PERSON ADDRESS", PERSON.getAddress());
+console.log("PERSON SUMMARY", PERSON.getSummary());
