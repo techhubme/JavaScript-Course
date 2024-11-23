4d8751c4ec0b173604454829fcc584c56f946d6b
Ram Niwash <rniwash122@gmail.com>
2024-11-23 18:35:26 +0530

    Added Lab 22, Index.html and .prettierrc file

--- Labs/js/lab21.js
+++ Labs/js/lab21.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
 /** Example for while loop */
 console.log("******* Example showing use of while loop *******");
 let iteration = 0;
 
-while(iteration <= 10){
-    console.log("ITERATION =",iteration);
-    iteration++;
+while (iteration <= 10) {
+  console.log("ITERATION =", iteration);
+  iteration++;
 }
 
 /** Example to show the real use case of while loop */
@@ -14,12 +14,12 @@ console.log("*******  Example to show the real use case of while loop *******");
 
 let dice = Math.trunc(Math.random() * 6) + 1;
 
-while(dice!=6){
-    console.log("DICE =",dice);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if(dice==6){
-        console.log("LOOP IS TERMINATING");
-    }
+while (dice != 6) {
+  console.log("DICE =", dice);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice == 6) {
+    console.log("LOOP IS TERMINATING");
+  }
 }
 console.log(`Got the required dice value ${dice}`);
 
@@ -28,7 +28,7 @@ console.log("*******  Example for do while loop *******");
 
 // let x = 1;
 let x = 11;
-do{
-    console.log("X = ", x);
-    x++;
-}while(x < 10);
+do {
+  console.log("X = ", x);
+  x++;
+} while (x < 10);
---
+++ Labs/js/lab22.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let x = 10;
+
+let y = x => {
+  return 'Hello' + x;
+};
+
+let myVarX = 'Hello World';
+
+console.log(myVarX);
+
+//FIXME BUG TODO VIDEO LOOKUP
