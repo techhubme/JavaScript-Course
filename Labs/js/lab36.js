4791436216afb0385736ff1defc64cacaa998a01
Ram Niwash <rniwash122@gmail.com>
2025-01-30 22:05:10 +0530

    Added Lab36 and Lab37

---
+++ Labs/js/lab36.js
@@ -0,0 +1,128 @@
+'use strict';
+
+/** The RESTAURANT Constant */
+const RESTAURANT = {
+  name: 'Bhartiya_Restro',
+  location: 'Near Sagwan Chonk, Sirsa',
+
+  categories: [
+    'Delhi Khana',
+    'Kerela Special',
+    'Punjabi Roti',
+    'Karnataka Dosa',
+  ],
+
+  starterMenu: ['Roti', 'Sabji', 'Salad', 'Paratha'],
+  mainMenu: ['Special', 'Lunch', 'Dinner', 'Breakfast'],
+
+  openingHours: {
+    saturday: {
+      open: 9,
+      close: 22,
+    },
+    sunday: {
+      open: 9,
+      close: 22,
+    },
+    monday: {
+      open: 6,
+      close: 22,
+    },
+    tuesday: {
+      open: 6,
+      close: 22,
+    },
+    wednesday: {
+      open: 6,
+      close: 22,
+    },
+    thursday: {
+      open: 6,
+      close: 22,
+    },
+    friday: {
+      open: 6,
+      close: 22,
+    },
+  },
+
+  /**
+   * Order food
+   *
+   * @param {number} starterIndex
+   * @param {number} mainIndex
+   * @returns
+   */
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  /**
+   * orderDelivery with multiple parameters to receive as object
+   *
+   * @param {object} param0
+   */
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log('---------- orderDelivery -------------');
+    console.log(`Order received! ${this.starterMenu[starterIndex]}
+      and ${this.mainMenu[mainIndex]} will be delivered to ${address}
+      at ${time}`);
+  },
+
+  /**
+   * orderDeliveryDefault with default parameter value.
+   *
+   * @param {object} param0
+   */
+  orderDeliveryDefault: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log('---------- orderDeliveryDefault -------------');
+    console.log(`Order received! ${this.starterMenu[starterIndex]}
+      and ${this.mainMenu[mainIndex]} will be delivered to ${address}
+      at ${time}`);
+  },
+
+  /**
+   * Order Pasta with differenct three ingredients
+   *
+   * @param {string} ing1
+   * @param {string} ing2
+   * @param {string} ing3
+   */
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  /**
+   * Order Pizza
+   *
+   * @param {string} mainIngredient
+   * @param  {...string} otherIngredients
+   */
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log('mainIngredient =', mainIngredient);
+    console.log('otherIngredients =', otherIngredients);
+  },
+};
+
+/**
+ * The Nullish Coalescing Operator (??)
+ *
+ * Introducted in ES 2020
+ *
+ * Nullish: null and undefined (NOT 0 or '')
+ */
+RESTAURANT.numGuests = 0;
+
+const guests = RESTAURANT.numGuests || 10;
+console.log('guests =', guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = RESTAURANT.numGuests ?? 10;
+console.log('guestCorrect =', guestCorrect);
---
+++ Labs/js/lab37.js
@@ -0,0 +1,142 @@
+'use strict';
+
+/** The RESTAURANT Constant */
+const RESTAURANT = {
+  name: 'Bhartiya_Restro',
+  location: 'Near Sagwan Chonk, Sirsa',
+
+  categories: [
+    'Delhi Khana',
+    'Kerela Special',
+    'Punjabi Roti',
+    'Karnataka Dosa',
+  ],
+
+  starterMenu: ['Roti', 'Sabji', 'Salad', 'Paratha'],
+  mainMenu: ['Special', 'Lunch', 'Dinner', 'Breakfast'],
+
+  openingHours: {
+    saturday: {
+      open: 9,
+      close: 22,
+    },
+    sunday: {
+      open: 9,
+      close: 22,
+    },
+    monday: {
+      open: 6,
+      close: 22,
+    },
+    tuesday: {
+      open: 6,
+      close: 22,
+    },
+    wednesday: {
+      open: 6,
+      close: 22,
+    },
+    thursday: {
+      open: 6,
+      close: 22,
+    },
+    friday: {
+      open: 6,
+      close: 22,
+    },
+  },
+
+  /**
+   * Order food
+   *
+   * @param {number} starterIndex
+   * @param {number} mainIndex
+   * @returns
+   */
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  /**
+   * orderDelivery with multiple parameters to receive as object
+   *
+   * @param {object} param0
+   */
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log('---------- orderDelivery -------------');
+    console.log(`Order received! ${this.starterMenu[starterIndex]}
+      and ${this.mainMenu[mainIndex]} will be delivered to ${address}
+      at ${time}`);
+  },
+
+  /**
+   * orderDeliveryDefault with default parameter value.
+   *
+   * @param {object} param0
+   */
+  orderDeliveryDefault: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log('---------- orderDeliveryDefault -------------');
+    console.log(`Order received! ${this.starterMenu[starterIndex]}
+      and ${this.mainMenu[mainIndex]} will be delivered to ${address}
+      at ${time}`);
+  },
+
+  /**
+   * Order Pasta with differenct three ingredients
+   *
+   * @param {string} ing1
+   * @param {string} ing2
+   * @param {string} ing3
+   */
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}.`
+    );
+  },
+
+  /**
+   * Order Pizza
+   *
+   * @param {string} mainIngredient
+   * @param  {...string} otherIngredients
+   */
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log('mainIngredient =', mainIngredient);
+    console.log('otherIngredients =', otherIngredients);
+  },
+};
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Sumaan',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log('rest1 =', rest1);
+console.log('rest2 =', rest2);
